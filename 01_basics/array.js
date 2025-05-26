3ab29bd2d516856d4dd7d066235f161fe4e970db
nanwani-karan <nanwanik21@gmail.com>
2025-05-26 08:28:30 +0000

    array2

--- 01_basics/array.js
+++ 01_basics/array.js
@@ -1,5 +1,18 @@
 const myarr=[0,1,2,3,4,5,]
-console.log(myarr[3]);
+//console.log(myarr[3]);
 const myheroes=["batman","doremon","nobita"]
+//myarr.push(6)
+//myarr.push(8)
+//console.log(myarr);
+//const newarr =myarr.join()
+//console.log(myarr);
+//console.log(typeof newarr);
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const useable_array = another_array.flat(Infinity)
+//console.log(useable_array);
+
+
+
+
 
 
