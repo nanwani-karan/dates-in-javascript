75b524c1e4ee3a9aa694d62d84fe127164fa846f
nanwani-karan <nanwanik21@gmail.com>
2025-05-26 07:42:32 +0000

    array and dates

---
+++ 01_basics/array.js
@@ -0,0 +1,5 @@
+const myarr=[0,1,2,3,4,5,]
+console.log(myarr[3]);
+const myheroes=["batman","doremon","nobita"]
+
+
---
+++ 01_basics/dates.js
@@ -0,0 +1,14 @@
+let newdate = new Date()
+console.log(newdate);
+console.log(newdate.getDate());
+console.log(newdate.getDay());
+console.log(newdate.getFullYear());
+console.log(newdate.getHours());
+console.log(newdate.getMinutes());
+console.log(newdate.getMonth());
+
+
+
+
+
+
